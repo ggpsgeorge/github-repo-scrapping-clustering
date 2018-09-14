694ed1c2d186187abf3e9d253c802689f1e56298
Kfourit <kfourit@gmail.com>
2018-09-13 23:37:18 -0300

    begginning json

--- minerador/sample/test.py
+++ minerador/sample/test.py
@@ -1,5 +1,6 @@
 from view_model import ViewModel
 import os
+import json
 
 
 token = ""
@@ -8,6 +9,6 @@ path2 = "https://api.github.com/repos/ggpsgeorge/minerador"
 path3 = "https://api.github.com/repos/Kfourit/test2"
 viewRepository = ViewModel(token)
 repository = viewRepository.getRepositoryFromPath(path3)
-#feature = viewRepository.get_feature_information(os.getcwd() + os.sep + "dados" + os.sep + "action_item.feature")
-print(repository)
-#print(feature)
+#print(repository)
+
+#print(json.dumps(repository.__dict__))
